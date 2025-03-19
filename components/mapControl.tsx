61f578009af2e22e06e3a58dd8861bc801dc0568
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-03-19 13:10:50 +0200

    Layout changes

--- components/mapControl.tsx
+++ components/mapControl.tsx
@@ -1,28 +1,32 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
+import { ThemedText } from "./ThemedText";
 
 export default function MapControl() {
   return (
     <View style={styles.flex}>
-      <Pressable
-        style={({ pressed }) => [
-          styles.addBtn,
-          styles.btn,
-          pressed && styles.pressed,
-        ]}
-        onPress={() => console.log("Add Location")}
-      >
-        <Text style={styles.text}>ADD LOCATION</Text>
-      </Pressable>
-      <Pressable
-        style={({ pressed }) => [
-          styles.clearBtn,
-          styles.btn,
-          pressed && styles.pressed,
-        ]}
-        onPress={() => console.log("Clear Location")}
-      >
-        <Text style={styles.text}>CLEAR LOCATION</Text>
-      </Pressable>
+      <ThemedText>Hello</ThemedText>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.addBtn,
+            styles.btn,
+            pressed && styles.pressed,
+          ]}
+          onPress={() => console.log("Add Location")}
+        >
+          <Text style={styles.text}>ADD LOCATION</Text>
+        </Pressable>
+        <Pressable
+          style={({ pressed }) => [
+            styles.clearBtn,
+            styles.btn,
+            pressed && styles.pressed,
+          ]}
+          onPress={() => console.log("Clear Location")}
+        >
+          <Text style={styles.text}>CLEAR LOCATION</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -31,9 +35,19 @@ const styles = StyleSheet.create({
   flex: {
     flex: 0.3,
     width: "100%",
+    height: 100,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "column",
+    borderTopColor: "#DAD9D9",
+    borderTopWidth: 1,
+    paddingTop: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "row",
+    marginTop: 10,
   },
   addBtn: {
     backgroundColor: "#16C1E8",
