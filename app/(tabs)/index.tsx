b6ac1980290803a81ee27b45168b77986283a8ce
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-03-19 10:55:15 +0200

    Layout for map page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
-import { ThemedText } from "@/components/ThemedText";
 import { StyleSheet } from "react-native";
+import { View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return <ThemedText style={styles.text}>Hello</ThemedText>;
+  return (
+    <SafeAreaProvider>
+      <View style={styles.flex} id="map" />
+      <View style={styles.flex} id="control" />
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
   text: {
     color: "black",
     fontSize: 20,
+  },
+  flex: {
+    flex: 1,
   },
 });
