cfa20fff5622df18cb24fb6e5c5d8aed6fa594cc
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-03-19 10:50:17 +0200

    Remove boilerplate code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,13 @@
 import { ThemedText } from "@/components/ThemedText";
+import { StyleSheet } from "react-native";
 
 export default function HomeScreen() {
-  return <ThemedText>Hello</ThemedText>;
+  return <ThemedText style={styles.text}>Hello</ThemedText>;
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: "black",
+    fontSize: 20,
+  },
+});
