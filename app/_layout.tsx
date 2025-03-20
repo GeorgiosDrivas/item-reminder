72cfbf36dc40e882f7befc769590a9c6b1e413dc
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-03-20 17:34:27 +0200

    Items list component and react native gesture handler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,14 @@
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 import HomeScreen from "./(tabs)";
+import TextFieldScreen from "./itemsList";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import { View, StyleSheet } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -10,6 +16,8 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  const [screen, setScreen] = useState("home");
 
   useEffect(() => {
     if (loaded) {
@@ -21,5 +29,27 @@ export default function RootLayout() {
     return null;
   }
 
-  return <HomeScreen />;
+  const onGestureEvent = (event: any) => {
+    if (event.nativeEvent.translationX > 50) {
+      setScreen("home");
+    } else if (event.nativeEvent.translationX < -50) {
+      setScreen("textField");
+    }
+  };
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <View style={styles.container}>
+          {screen === "home" ? <HomeScreen /> : <TextFieldScreen />}
+        </View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
