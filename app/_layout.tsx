d71136342dd162bb9a0ebe18d8b8e9753de101e6
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-03-26 15:26:34 +0200

    Replace Gesture with Modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
-import "react-native-reanimated";
+import { StyleSheet, View, Modal, Pressable, Text } from "react-native";
 import HomeScreen from "./(tabs)";
-import { StyleSheet, View } from "react-native";
 import ItemsList from "./(tabs)/itemsList";
 
 export default function RootLayout() {
-  const [screen] = useState("home");
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <View style={styles.container}>
-      {screen === "home" ? <HomeScreen /> : <ItemsList />}
+      <HomeScreen />
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <ItemsList />
+        <Pressable
+          onPress={() => setModalVisible(false)}
+          style={styles.closeButton}
+        >
+          <Text style={styles.closeText}>Close</Text>
+        </Pressable>
+      </Modal>
+      <Pressable
+        onPress={() => setModalVisible(true)}
+        style={styles.itemsListButton}
+      >
+        <Text>ITEMS LIST</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,5 +35,23 @@ export default function RootLayout() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  closeButton: {
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: "#FF6347",
+    borderRadius: 5,
+  },
+  closeText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  itemsListButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    padding: 10,
+    backgroundColor: "transparent",
+    borderRadius: 5,
   },
 });
