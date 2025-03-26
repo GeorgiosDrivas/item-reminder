547a4a86a0f3bda0e7f55173b64e06560bb349ff
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-03-26 15:28:31 +0200

    Comment out notification functionality until finished

--- app/(tabs)/itemsList.tsx
+++ app/(tabs)/itemsList.tsx
@@ -1,9 +1,8 @@
 import ListControl from "@/components/list/listControl";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { indexStyles, listStyles } from "../style";
 import { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { sendNotification } from "../notification";
 
 export default function ItemsList() {
   const [itemsList, setItemsList] = useState<string[]>([]);
@@ -62,10 +61,10 @@ export default function ItemsList() {
       <View style={indexStyles.flex}>
         <ListControl addItem={addItem} clearItems={clearItems} />
       </View>
-      <Button
+      {/* <Button
         title="Send Notification"
         onPress={() => sendNotification({ items: itemsList })}
-      />
+      /> */}
     </>
   );
 }
